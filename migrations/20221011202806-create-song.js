f909bbaacd287aaf02820c56ca7d5489a1759c37
yasminnabila <yasmnnabila@gmail.com>
2022-10-12 03:30:27 +0700

    Migrate tables

---
+++ migrations/20221011202806-create-song.js
@@ -0,0 +1,37 @@
+"use strict";
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Songs", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      songId: {
+        allowNull: false,
+        type: Sequelize.STRING,
+      },
+      PlaylistId: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: "Playlists",
+          key: "id",
+        },
+        onUpdate: "cascade",
+        onDelete: "cascade",
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Songs");
+  },
+};
