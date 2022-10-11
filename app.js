71562a043e84d13aefe59d8d5f3256be3c510f75
yasminnabila <yasmnnabila@gmail.com>
2022-10-12 03:24:45 +0700

    Setup express

---
+++ app.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+const cors = require("cors");
+const express = require("express");
+const app = express();
+const routes = require("./routes");
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/", routes);
+
+app.listen(PORT, () => {
+  console.log(`iProject is on PORT ${PORT}`);
+});
+
+module.exports = app;
