03d9435f6e37c912e0ef129cc2640b266e5df9d2
yasminnabila <yasmnnabila@gmail.com>
2022-10-13 06:51:20 +0700

    Revise api routes

--- routes/spotify-api.js
+++ routes/spotify-api.js
@@ -7,5 +7,6 @@ router.get("/get-song", spotifyController.getSongById);
 router.get("/get-album", spotifyController.getAlbumById);
 router.get("/get-artist", spotifyController.getArtistById);
 router.get("/search-song", spotifyController.searchSong);
+router.get("/get-song-audio-features", spotifyController.getAudioFeatures);
 
 module.exports = router;
