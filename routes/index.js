a7becb5a4cd1a2ccd3759df2c87ca984353fe0d6
yasminnabila <yasmnnabila@gmail.com>
2022-10-12 15:50:08 +0700

    Route playlist done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const user = require("./user");
+const playlist = require('./')
 const ErrorHandler = require("../middlewares/ErrorHandler");
 
 router.use("/", user);
---
+++ routes/playlist.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const playlistController = require("../controller/playlistController");
+
+
+
+module.exports = router;
