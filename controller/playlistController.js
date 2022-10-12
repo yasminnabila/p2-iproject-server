7eb9e44d3f9a4ef222b22d401fa8fd9ff3d1b271
yasminnabila <yasmnnabila@gmail.com>
2022-10-12 17:05:13 +0700

    Read playlist detail

--- controller/playlistController.js
+++ controller/playlistController.js
@@ -1,3 +1,4 @@
+const spotify = require("../helpers/spotify");
 const { Playlist, Song } = require("../models");
 
 class playlistController {
@@ -23,8 +24,8 @@ class playlistController {
     try {
       const { id } = req.user;
       const { name, imageUrl } = req.body;
-      console.log(req.user, "INI REQ USER");
-      console.log(req.body, "INI REQ BODY");
+      // console.log(req.user, "INI REQ USER");
+      // console.log(req.body, "INI REQ BODY");
 
       await Playlist.create({
         name,
@@ -34,6 +35,41 @@ class playlistController {
       res.status(201).json({
         statusCode: 201,
         message: "Playlist created successfully",
+      });
+    } catch (error) {
+      console.log(error);
+      next(error);
+    }
+  }
+
+  static async readPlaylistById(req, res, next) {
+    try {
+      const { playlistId } = req.params;
+      let playlist = await Song.findAll({
+        where: {
+          PlaylistId: playlistId,
+        },
+        include: {
+          model: Playlist,
+        },
+      });
+      let song = "";
+      if (playlist.length === true) {
+        let songs = playlist.map((el) => el.songId);
+        const spotifyApi = await spotify();
+        song = await spotifyApi.getTracks(songs);
+        console.log(song);
+        song.body.tracks.forEach((el, i) => {
+          el.disc_number = playlist[i].id;
+        });
+        song.body.playlist = playlist[0].Playlist;
+      } else {
+        song = {
+          body: [],
+        };
+      }
+      res.status(200).json({
+        data: song.body,
       });
     } catch (error) {
       console.log(error);
--- routes/playlist.js
+++ routes/playlist.js
@@ -5,5 +5,6 @@ const { isLoggedIn, isAuthorized } = require("../middlewares/Authz");
 router.use(isLoggedIn);
 router.get("/", playlistController.readAllPlaylist);
 router.post("/", playlistController.addPlaylist);
+router.get("/:playlistId", playlistController.readPlaylistById);
 
 module.exports = router;
