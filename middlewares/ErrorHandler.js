b5e39f33785ca0821a28574af0ac78097b6e8eef
yasminnabila <yasmnnabila@gmail.com>
2022-10-12 15:40:48 +0700

    Check errors for authz in postman

--- middlewares/ErrorHandler.js
+++ middlewares/ErrorHandler.js
@@ -7,9 +7,12 @@ module.exports = (error, req, res, next) => {
   ) {
     code = 400;
     msg = error.errors[0].message;
+  } else if (error.name === "JsonWebTokenError") {
+    code = 401;
+    msg = "Invalid token";
   } else if (error.name === "SequelizeDatabaseError") {
     code = 404;
-    msg = "Data is not found";
+    msg = "Data not found";
   } else if (error.code) {
     code = error.code;
     msg = error.msg;
--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,6 @@ const { isLoggedIn } = require("../middlewares/Authz");
 
 router.post("/register", userController.register);
 router.post("/login", userController.login);
-router.get("me", isLoggedIn, userController.me);
+router.get("/me", isLoggedIn, userController.me);
 
 module.exports = router;
