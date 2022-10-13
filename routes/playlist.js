f89e2b5329d314905e834bfc3b284f72a32b7f98
yasminnabila <yasmnnabila@gmail.com>
2022-10-13 14:00:23 +0700

    Edit authentication

--- routes/playlist.js
+++ routes/playlist.js
@@ -2,9 +2,9 @@ const router = require("express").Router();
 const playlistController = require("../controller/playlistController");
 const { Authentication, Authorized } = require("../middlewares/Authz");
 
-router.use(Authentication);
 router.get("/", playlistController.readAllPlaylist);
 router.post("/", playlistController.addPlaylist);
+router.use(Authentication);
 router.get("/:playlistId", playlistController.readPlaylistById);
 router.delete("/:playlistId", Authorized, playlistController.deletePlaylist);
 router.patch(
